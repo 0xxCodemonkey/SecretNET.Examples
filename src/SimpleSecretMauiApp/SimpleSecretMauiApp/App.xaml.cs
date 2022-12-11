06e07c3a4798650b176a8b86b0511eb03fda2faf
0xxCodemonkey <codemonkey@semantico.de>
2022-12-11 05:24:06 +0100

    Init app project

---
+++ src/SimpleSecretMauiApp/SimpleSecretMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleSecretMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/SimpleSecretMauiApp/SimpleSecretMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleSecretMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
